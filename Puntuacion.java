e73d03cf03360243031e50cf03c83d561b0700d2
EC2 Default User <ec2-user@ip-172-31-94-63.ec2.internal>
2022-06-12 16:45:07 +0000

    agregamos

---
+++ Puntuacion.java
@@ -0,0 +1,14 @@
+/**
+ * Class puntuación
+ */
+public class puntuación {
+	private String nombre;
+	private int calificación; //alta 1000/baja 300
+	
+	
+	//Metodos
+	public void calificar() } //a  los jugadores por parte del jurado
+	}
+	public void sacar ganadores() } //los 3 primeros lugares
+	}
+}
