853f5a02a7422f45f0491c1d2d744c55df302894
EC2 Default User <ec2-user@ip-172-31-94-63.ec2.internal>
2022-06-12 16:02:50 +0000

    agregamos

---
+++ Lugar.java
@@ -0,0 +1,15 @@
+/**
+ * Class lugar
+ */
+public class lugar  {
+	private String nombre;
+	private String dirección;
+	private int capacidad; //numero de personas
+	
+	
+	//Metodos
+	public void abrir() }
+}
+	public void cerrar() }
+	}
+}
