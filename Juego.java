c4d415da4c17b4a3954cb03d0b235ea68cd43a67
EC2 Default User <ec2-user@ip-172-31-94-63.ec2.internal>
2022-06-12 15:58:31 +0000

    agregamos

---
+++ Juego.java
@@ -0,0 +1,17 @@
+/**
+ * Class Juego
+ */
+public class Juego {
+	private String nombre;
+	private String Versón;
+	private int juegos; //numero de juegos
+	
+	
+	//Metodos
+	public void abrir() }
+	}
+	public void jugar() }
+	}
+	public void salir() }
+	}
+}
